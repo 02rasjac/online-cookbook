6af75d8113b5c87f4111871ecf26923f2c782308
02rasjac <rasmusjacklin2002@gmail.com>
2021-02-26 14:11:51 +0100

    Update usertable to include correct fields

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,6 +11,18 @@ class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
+    const ROLE_USER     = 'user';
+    const ROLE_VERIFIED = 'verified';
+    const ROLE_STAFF    = 'staff';
+    const ROLE_ADMIN    = 'admin';
+
+    const ROLE_CHOICES = [
+        User::ROLE_USER, 
+        User::ROLE_VERIFIED, 
+        User::ROLE_STAFF,
+        User::ROLE_ADMIN
+    ];
+
     /**
      * The attributes that are mass assignable.
      *
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,6 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+use App\Models\User;
+
 class CreateUsersTable extends Migration
 {
     /**
@@ -15,10 +17,15 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->unique();
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', User::ROLE_CHOICES)->default(User::ROLE_USER);
+            $table->boolean('recipies_is_public')->default('true');
+            $table->string('profile_pic')->default('default_profile_pic.jpeg');
             $table->rememberToken();
             $table->timestamps();
         });
