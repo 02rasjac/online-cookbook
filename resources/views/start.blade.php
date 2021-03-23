3bfa32e9c402c50ce093efbfc685b9fdac74dac4
02rasjac <rasmusjacklin2002@gmail.com>
2021-03-23 14:06:31 +0100

    Add info-box about recipie

--- resources/views/start.blade.php
+++ resources/views/start.blade.php
@@ -1,17 +1,26 @@
 @extends('layouts.landing')
 
 @section('content')
-  <div class="hero">
+  <header class="hero">
     <img class="hero-image" src="{{ Storage::url('images/food/hero.jpg') }}" alt="(title of the recipie)">
-    <div class="container content">
-      <h1>
-        Din <strong>kokbok</strong>: Online <br>
-        Din <strong>måltidsplanerare</strong>: Online
-      </h1>
-      <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente consectetur animi omnis.
-      </p>
-      <a href="#about" class="primary-btn">Läs Mer</a>
-      <a href="{{ route('register') }}" class="secondary-btn">Skapa Konto</a>
-    </div>
-  </div>
+    <div class="container">
+      <section class="content">
+        <h1>
+          Din <strong>kokbok</strong>: Online <br>
+          Din <strong>måltidsplanerare</strong>: Online
+        </h1>
+        <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente consectetur animi omnis.
+        </p>
+        <a href="#about" class="primary-btn">Läs Mer</a>
+        <a href="{{ route('register') }}" class="secondary-btn">Skapa Konto</a>
+      </section>
+      <div class="recipe">
+        <img src="{{ Storage::url('images/profile-images/local-profile-image.png') }}" alt="">
+        <div class="info">
+          <p class="recipie-owner">Recept Ägare</p>
+          <p class="recipie-title">Recept Titel</p>
+        </div>
+      </div>
+
+  </header>
 @endsection
