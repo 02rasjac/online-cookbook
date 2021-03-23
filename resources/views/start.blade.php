360dec919fd5bd337d844ed58b04efc7aa7d3515
02rasjac <rasmusjacklin2002@gmail.com>
2021-03-23 10:32:41 +0100

    Positioned content correctly

--- resources/views/start.blade.php
+++ resources/views/start.blade.php
@@ -8,7 +8,8 @@
         Din <strong>kokbok</strong>: Online <br>
         Din <strong>måltidsplanerare</strong>: Online
       </h1>
-      <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente consectetur animi omnis.</p>
+      <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente consectetur animi omnis.
+      </p>
       <a href="#about" class="primary-btn">Läs Mer</a>
       <a href="{{ route('register') }}" class="secondary-btn">Skapa Konto</a>
     </div>
