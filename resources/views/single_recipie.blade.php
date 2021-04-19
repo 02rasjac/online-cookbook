acf5d0184f9f36f4c82a50d464cb0bfcb3ad032e
Rasmus Jacklin <rasmusjacklin2002@gmail.com>
2021-04-19 10:15:45 +0200

    Style ingredients

--- resources/views/single_recipie.blade.php
+++ resources/views/single_recipie.blade.php
@@ -1,37 +1,46 @@
 @extends('layouts.app')
 
 @section('content')
-  <h1>{{ $recipie->title }}</h1>
-  <section>
-    <p><i class="fas fa-clock"></i> {{ $recipie->cook_time }}</p>
-    <p><i class="fab fa-apple"></i> {{ $recipie->numOfIngredients() }}</p>
-    <p>{{ $recipie->difficulty }}</p>
-  </section>
-  <p>{{ $recipie->description }}</p>
+  <div class="container" id="recipie">
+    <header class="my-4">
+      <h1 class="text-capitalize">{{ $recipie->title }}</h1>
+      <section class="d-flex">
+        <p class="me-3"><i class="fas fa-clock"></i> {{ $recipie->cook_time }} min</p>
+        <p class="me-3"><i class="fab fa-apple"></i> {{ $recipie->numOfIngredients() }} ingredienser</p>
+        <p class="me-3">{{ $recipie->difficulty }}</p>
+      </section>
+    </header>
 
-  <section>
-    <h2>Instruktioner</h2>
-    @foreach ($recipie->instruction as $instruction)
-        <p>{{ $instruction->text }}</p>
-        @if ($instruction->timer !== null)
-            <button>Timer: {{ $instruction->timer }} min</button>
-        @endif
-    @endforeach
-  </section>
+    <section class="row">
+      <section class="col-sm-8">
+        <p class="fw-bold">{{ $recipie->description }}</p>
 
-  @if ($recipie->ingredientGroup)
-    <aside>
-      @foreach ($recipie->ingredientGroup as $group)
-        <section>
-          <h3>{{ $group->title }}</h3>
-          <ul>
-            @foreach ($group->groupIngredient as $item)
-              <li>{{ $item->ingredient->ingredient_name }} | {{ $item->quantity }}
-                {{ $item->measurementUnit->measurement_name }}</li>
-            @endforeach
-          </ul>
-        </section>
-      @endforeach
-    </aside>
-  @endif
+        <h2>Instruktioner</h2>
+        @foreach ($recipie->instruction as $instruction)
+            <p>{{ $instruction->text }}</p>
+            @if ($instruction->timer !== null)
+                <button>Timer: {{ $instruction->timer }} min</button>
+            @endif
+        @endforeach
+      </section>
+  
+      @if ($recipie->ingredientGroup)
+        <aside class="col-md-4">
+          @foreach ($recipie->ingredientGroup as $group)
+            <section class="mb-4">
+              <ul class="list-group ingredient-group">
+                <li class="list-group-item bg-primary text-center text-light group-title"><h3 class="fw-bold">{{ $group->title }}</h3></li>
+                @foreach ($group->groupIngredient as $item)
+                  <li class="list-group-item border border-primary d-flex justify-content-between">
+                    <p class="fw-bold">{{ $item->ingredient->ingredient_name }}</p>
+                    <p>{{ $item->quantity }} {{ $item->measurementUnit->measurement_name }}</p>
+                  </li>
+                @endforeach
+              </ul>
+            </section>
+          @endforeach
+        </aside>
+      @endif
+    </section>
+  </div>
 @endsection
