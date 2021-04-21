a1248ba7e5109f26181274971cf834145e75caf6
02rasjac <rasmusjacklin2002@gmail.com>
2021-04-21 14:44:17 +0200

    Add error on attempt to fetch recipie to wrong user

--- app/Http/Controllers/RecipieController.php
+++ app/Http/Controllers/RecipieController.php
@@ -5,12 +5,23 @@ namespace App\Http\Controllers;
 use App\Models\GroupIngredient;
 use App\Models\IngredientGroup;
 use App\Models\Recipie;
+use App\Models\User;
+use Illuminate\Http\Response;
 use Illuminate\Http\Request;
 
 class RecipieController extends Controller
 {
+    /**
+     * Get the requested recipie's data
+     */
     public function showRecipie(Request $request, $username, $recipie_id) {
         $recipie = Recipie::find($recipie_id);
-        return view('single_recipie', ['recipie' => $recipie]);
+
+        // Make sure it's the correct user-owner
+        if ($recipie->user->name === $username) {
+            return view('single_recipie', ['recipie' => $recipie]);
+        }
+
+        abort(404);
     }
 }
