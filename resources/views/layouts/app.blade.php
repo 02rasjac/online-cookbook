ae0b610bc341c7ee0214011bed6fa2a131216bc2
Rasmus Jacklin <rasmusjacklin2002@gmail.com>
2021-05-11 10:28:46 +0200

    Add icon in head

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,9 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" href="{{ Storage::url('images/small-logo.svg') }}">
+
+  <title>Kokboken Online: Startsida</title>
 
   <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
