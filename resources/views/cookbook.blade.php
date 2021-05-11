59a185fd8615387903d9a373c97d12c248c1cf17
Rasmus Jacklin <rasmusjacklin2002@gmail.com>
2021-05-11 09:55:11 +0200

    Add link to recipie

--- resources/views/cookbook.blade.php
+++ resources/views/cookbook.blade.php
@@ -17,34 +17,39 @@
       <section>
         <div class="d-flex flex-wrap justify-content-around">
           @foreach ($recipies as $recipie)
-            <article class="card mb-4 recipie-info">
-              <div class="thumbnail">
-                <img src="{{ Storage::url($recipie->thumbnail) }}" class="card-img-top">
-              </div>
-              <div class="card-body rounded-top rounded-4">
-                <h5 class="text-capitalize card-title text-center">{{ $recipie->title }}</h5>
-                <section class="d-flex mt-2">
-                  <p class="me-3"><i class="fas fa-clock"></i> {{ $recipie->cook_time }} min</p>
-                  <p class="me-3"><i class="fab fa-apple"></i> {{ $recipie->numOfIngredients() }} ingredienser</p>
-                  <p class="me-3">{{ $recipie->difficulty }}</p>
-                </section>
-                <ul class="list-group list-group-horizontal mt-2 tags">
-                @foreach ($recipie->recipieTags as $tag)
-                  <li class="list-group-item rounded-pill tag" style="background-color: {{ $tag->tag->color }}">{{ $tag->tag->tag_name }}</li>
-                @endforeach
-                </ul>
-                <p class="card-text mt-3">{{ $recipie->description }}</p>
-                <div class="d-flex justify-content-between align-items-end fs-4 cookbook-links">
-                  <a href="{{ route('edit-recipie', ['username' => Auth::user()->name, 'recipie_id' => $recipie->id]) }}">
-                    <i class="fas fa-pencil-alt text-secondary" title="Redigera"></i>
-                  </a>
-                  <p class="fs-6"><i class="fas fa-star text-secondary"></i> 4.5</p>
-                  <a href="{{ route('favorite', $recipie->id) }}">
-                    <i class="far fa-heart text-black-50" title="Spara"></i>
-                  </a>
+              <article class="card mb-4 recipie-info">
+                {{-- <a href="{{ route('recipie', [$recipie->user->name, $recipie->id]) }}" class="recipie-link">
+                  <span></span>
+                </a> --}}
+                <div class="thumbnail">
+                  <img src="{{ Storage::url($recipie->thumbnail) }}" class="card-img-top">
                 </div>
-              </div>
-            </article>
+                <div class="card-body rounded-top rounded-4">
+                  <a href="{{ route('recipie', [$recipie->user->name, $recipie->id]) }}" class="recipie-link">
+                    <h5 class="text-capitalize card-title text-center">{{ $recipie->title }}</h5>
+                  </a>
+                  <section class="d-flex mt-2">
+                    <p class="me-3"><i class="fas fa-clock"></i> {{ $recipie->cook_time }} min</p>
+                    <p class="me-3"><i class="fab fa-apple"></i> {{ $recipie->numOfIngredients() }} ingredienser</p>
+                    <p class="me-3">{{ $recipie->difficulty }}</p>
+                  </section>
+                  <ul class="list-group list-group-horizontal mt-2 tags">
+                  @foreach ($recipie->recipieTags as $tag)
+                    <li class="list-group-item rounded-pill tag" style="background-color: {{ $tag->tag->color }}">{{ $tag->tag->tag_name }}</li>
+                  @endforeach
+                  </ul>
+                  <p class="card-text mt-3">{{ $recipie->description }}</p>
+                  <div class="d-flex justify-content-between align-items-end fs-4 cookbook-links">
+                    <a href="{{ route('edit-recipie', ['username' => Auth::user()->name, 'recipie_id' => $recipie->id]) }}">
+                      <i class="fas fa-pencil-alt text-secondary" title="Redigera"></i>
+                    </a>
+                    <p class="fs-6"><i class="fas fa-star text-secondary"></i> 4.5</p>
+                    <a href="{{ route('favorite', $recipie->id) }}">
+                      <i class="far fa-heart text-black-50" title="Spara"></i>
+                    </a>
+                  </div>
+                </div>
+              </article>
           @endforeach
         </div>
         <div class="mb-4">
